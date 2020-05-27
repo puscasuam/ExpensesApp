24ef4a3a5b6afb63b5cde86868afb88a61519c96
Ana <puscasu.a.m@gmail.com>
2020-05-27 13:25:39 +0300

    Fix - fixed missing require function

--- ExpensesApp/ClientApp/src/app/home/home.component.ts
+++ ExpensesApp/ClientApp/src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+declare var require: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
+
 export class HomeComponent {
 
   public heroImageUrl = require("./images/expense.jpg");
